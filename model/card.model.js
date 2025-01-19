1332f89c93ac434f40745e2e6ddf526cdb79c1ec
Shlomo Daniel Gindi <shlomodani054@gmail.com>
2025-01-20 00:32:15 +0200

    cards model and joi validation

--- model/card.model.js
+++ model/card.model.js
@@ -3,35 +3,52 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const cardSchema = new mongoose.Schema({
-  bizName: {
+  title: {
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 255,
+    maxlength: 50,
   },
-  bizDescription: {
+  subtitle: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 500,
   },
-  bizAddress: {
+  description: {
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 255,
+    maxlength: 400,
   },
   bizPhone: {
     type: String,
     required: true,
     minlength: 9,
-    maxlength: 10,
+    maxlength: 11,
   },
-  bizImage: {
+  email: {
     type: String,
     required: true,
-    minlength: 2,
+    minlength: 5,
+    maxlength: 255,
+  },
+  web: {
+    type: String,
+    minlength: 5,
     maxlength: 500,
+  },
+  image: {
+    url: { type: String, minlength: 3, maxlength: 500 },
+    alt: { type: String, minlength: 2, maxlength: 500 },
+  },
+  address: {
+    state: { type: String, minlength: 2, maxlength: 255, required: true },
+    country: { type: String, minlength: 2, maxlength: 255 },
+    city: { type: String, minlength: 2, maxlength: 255 },
+    street: { type: String, minlength: 2, maxlength: 255 },
+    houseNumber: { type: Number, min: 2, max: 10 },
+    zip: { type: Number, min: 2, max: 15 },
   },
   bizNumber: {
     type: Number,
@@ -40,6 +57,12 @@ const cardSchema = new mongoose.Schema({
     max: 3_999_999_999,
     unique: true,
   },
+  likes: [
+    {
+      type: String,
+      ref: "User",
+    },
+  ],
 });
 
 const Card = mongoose.model("Card", cardSchema, "cards");
@@ -50,10 +73,10 @@ function validateCard(card) {
     subtitle: Joi.string().min(2).max(70).required(),
     description: Joi.string().min(2).max(400).required(),
     phone: Joi.string().min(9).max(11).required(),
-    email: Joi.string().min(2).max(255).email().required(),
-    web: Joi.string().min(5).max(255),
+    email: Joi.string().min(5).max(255).email().required(),
+    web: Joi.string().min(5).max(500),
     image: {
-      url: Joi.string().min(2).max(500),
+      url: Joi.string().min(3).max(500),
       alt: Joi.string().min(2).max(100),
     },
     address: {
@@ -64,7 +87,10 @@ function validateCard(card) {
       houseNumber: Joi.number().min(2).max(10),
       zip: Joi.number().min(2).max(15),
     },
-    likes: [],
+    likes: Joi.array()
+      .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/))
+      .unique()
+      .default([]),
   });
   return schema.validate(card);
 }
