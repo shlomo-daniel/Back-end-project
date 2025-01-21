32f5c2e6548deca2daa4f13c6b330fd7b1dca927
Shlomo Daniel Gindi <shlomodani054@gmail.com>
2025-01-21 05:45:59 +0200

    removed log

--- config/auth.config.js
+++ config/auth.config.js
@@ -4,8 +4,6 @@ function configError(message) {
   throw new Error(message);
 }
 
-console.log(process.env.JWT_KEY, "process.env.JWT_KEY");
-
 module.exports = {
   jwtKey:
     process.env.JWT_KEY ?? configError("envirment variable JWT_KEY is missing"),
